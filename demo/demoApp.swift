6bcde04bf45ef7ef6eb2037bf7ce192d39114544
Hritu Raj <sac3xhin@gmail.com>
2026-04-16 15:53:13 +0530

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Hritu Raj on 16/04/26.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
